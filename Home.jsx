0d5d0b703a2de0aaa63c05f478d3adaf4fdc3148
Ayushi-k-15 <159800975+Ayushi-k-15@users.noreply.github.com>
2024-02-13 00:16:06 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="item3">
+      <div className="mainPic">
+        <img src="/assets/nature/Sapling.png" alt="sapling" width="100%" />
+        {/* <div className="overview"> */}
+        <p className="overlapping-text">
+          Anything and Everything to MAKE and MAINTAIN your Garden ...
+        </p>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+}
